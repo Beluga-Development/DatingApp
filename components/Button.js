73f334e712e3fc6dda9e36062d1bde48dc227b8e
Mohmd <m_alsaidi@fanshaweonline.ca>
2026-03-02 10:30:52 -0500

    New app based on what we did in mobile, if I forgot to delete anything lmk and delete it. Should be good to work off, didn't change all the color names because refactoring the names might cause issues. Need to make components for profiles and stuff. Will use firebase or sql for data.

---
+++ components/Button.js
@@ -0,0 +1,30 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { useState } from 'react';
+import { Pressable, Text } from 'react-native';
+
+import style from '../style.js';
+
+export default (props) => {
+
+    const [isPressed, setIsPressed] = useState(false);
+    const pressIn = () => setIsPressed(true);
+    const pressOut = () => setIsPressed(false);
+
+    let buttonStyle = isPressed ? style.buttonPressed : style.button;
+
+    // Style overwrites
+    buttonStyle = { ...buttonStyle, ...props.style };
+    let buttonTextStyle = { ...style.buttonText, ...props.textStyle};
+
+    return (
+        <Pressable style={buttonStyle} onPress={props.onPress} onPressIn={pressIn} onPressOut={pressOut}>
+            {
+                props?.icon
+                    ?
+                    <Ionicons name={props.icon.name} size={props.icon.size} color={style.icon.color} />
+                    : <Text style={buttonTextStyle}>{props.text}</Text>
+            }
+        </Pressable>
+    );
+}
---
+++ style.js
@@ -0,0 +1,113 @@
+import { StyleSheet } from "react-native";
+
+export const MARGIN = 10;
+
+export const palette = {
+    primary: '#ed3c89',
+    border: '#f1abc6',
+    text: '#828282'
+};
+
+export default StyleSheet.create({
+    header: {
+        fontSize: 24,
+        fontFamily: 'Bitcount',
+        color: palette.text,
+        textAlign: 'center'
+    },
+    list: {
+        marginTop: 4,
+        borderTopWidth: 4,
+        fontFamily: 'Bitcount',
+        borderBottomWidth: 4,
+        marginBottom: 4,
+        borderColor: palette.border,
+        backgroundColor: palette.primary,
+        flex: 1
+    },
+    app: {
+        marginTop: 50,
+        backgroundColor: palette.primary,
+        fontFamily: 'Bitcount',
+        flex: 1
+    },
+    prompt: {
+        margin: MARGIN * 4,
+        alignSelf: 'stretch',
+        borderWidth: 2,
+        borderRadius: 12,
+        borderBottomWidth: 4,
+        borderRightWidth: 4,
+        borderColor: palette.border,
+        backgroundColor: palette.primary,
+    },
+    promptText: {
+        margin: MARGIN,
+        fontSize: 20,
+        fontFamily: 'Bitcount',
+        color: palette.text,
+        textAlign: 'center'
+    },
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    button: {
+        margin: MARGIN,
+        borderWidth: 1,
+        borderBottomWidth: 4,
+        borderRightWidth: 4,
+        borderRadius: 12,
+        color: palette.text,
+        borderColor: palette.border,
+        backgroundColor: palette.primary,
+        padding: 10,
+        alignItems: 'center',
+        fontFamily: 'Bitcount',
+    },
+    buttonText: {
+        fontSize: 20,
+        fontFamily: 'Bitcount',
+        color: palette.text,
+        textAlign: 'center'
+    },
+    buttonPressed: {
+        margin: MARGIN,
+        marginTop: MARGIN + 2,
+        borderWidth: 1,
+        backgroundColor: palette.primary,
+        borderBottomWidth: 2,
+        borderRightWidth: 2,
+        borderRadius: 12,
+        borderColor: palette.border,
+        padding: 10,
+        alignItems: 'center',
+        fontFamily: 'Bitcount',
+    },
+    listItem: {
+        margin: MARGIN,
+        borderWidth: 2,
+        borderRadius: 12,
+        fontFamily: 'Bitcount',
+        borderColor: palette.border,
+        padding: 10
+    },
+    listItemText: {
+        fontSize: 24,
+        fontFamily: 'Bitcount',
+        color: palette.text
+    },
+    inputText: {
+        margin: MARGIN,
+        borderWidth: 2,
+        fontFamily: 'Bitcount',
+        borderRadius: 12,
+        borderColor: palette.border,
+        padding: 10,
+        fontSize: 24,
+        color: palette.text
+    },
+    icon: {
+        color: palette.text
+    }
+});
