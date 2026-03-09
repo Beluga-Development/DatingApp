b698e0a3e1c9eaa7f720199c539816d675315b79
Nicolas <midia71@outlook.com>
2026-03-09 15:35:44 -0400

    Some ui for profile page

--- DatingApp/app/(tabs)/profile.jsx
+++ DatingApp/app/(tabs)/profile.jsx
@@ -1,21 +1,30 @@
 import { router } from "expo-router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Button, Text, View } from "react-native";
 import { SessionContext } from "../../util/session";
 
 export default function ProfileScreen() {
   const { setIsLoggedIn } = useContext(SessionContext);
-
+  const [profileData, setProfileData] = useState();
   const handleLogout = () => {
     setIsLoggedIn(false);
     router.replace("/login");
   };
 
   return (
-    <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    >
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Profile</Text>
+      <Text
+        style={{
+          width: "80%",
+
+          backgroundColor: "#777777",
+          borderRadius: 12,
+          margin: 10,
+        }}
+      >
+        {profileData}
+      </Text>
       <Button title="Log Out" onPress={handleLogout} />
     </View>
   );
