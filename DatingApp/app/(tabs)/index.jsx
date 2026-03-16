6d4fbf79ac7adb84a86233b9e54ece4a9485c314
KwinSea <Deshawn.D.Brown.6@gmail.com>
2026-03-16 09:27:51 -0400

    Revert "added conditional rendering and matches array without info yet"
    This reverts commit 45f78e9f9f10dafec6c2df1019f889867f07385f.

--- DatingApp/app/(tabs)/index.jsx
+++ DatingApp/app/(tabs)/index.jsx
@@ -1,22 +1,11 @@
 import { Text, View } from "react-native";
 
-const [page, setPage] = useState("Home");
-//conditional rendering of app
-
 export default function HomeScreen() {
   return (
     <View
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      {page == "Home" && <Text>Home</Text>}
-      
-      {page == "Login" && <Text>Login</Text>}
-
-      {page == "Profile" && <Text>Your Profile</Text>}
-
-      {page == "Matches" && <Text>Your Matches</Text>}
-
-
+      <Text>Home</Text>
     </View>
   );
 }
