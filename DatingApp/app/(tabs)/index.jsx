45f78e9f9f10dafec6c2df1019f889867f07385f
Mohmd <m_alsaidi@fanshaweonline.ca>
2026-03-16 09:19:53 -0400

    added conditional rendering and matches array without info yet

--- DatingApp/app/(tabs)/index.jsx
+++ DatingApp/app/(tabs)/index.jsx
@@ -1,11 +1,22 @@
 import { Text, View } from "react-native";
 
+const [page, setPage] = useState("Home");
+//conditional rendering of app
+
 export default function HomeScreen() {
   return (
     <View
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <Text>Home</Text>
+      {page == "Home" && <Text>Home</Text>}
+      
+      {page == "Login" && <Text>Login</Text>}
+
+      {page == "Profile" && <Text>Your Profile</Text>}
+
+      {page == "Matches" && <Text>Your Matches</Text>}
+
+
     </View>
   );
 }
--- DatingApp/app/(tabs)/matches.jsx
+++ DatingApp/app/(tabs)/matches.jsx
@@ -1,11 +1,35 @@
 import { Text, View } from "react-native";
 
-export default function MatchesScreen() {
+const Matches = ({matches}) => {
+
+if(!matches)
+  return(<>
+  </>)
+
+
+const items = []; // will push matches into a match array, 
+// TODO set data
+  for (const match of matches) {
+    items.push(
+        <Paper elevation={4} sx={{ marginTop: "0.5em" }}>
+      <CardContent key={match.id}>
+      </CardContent>
+      </Paper>
+    );
+  }
+
+
+
+
   return (
     <View
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
       <Text>Matches</Text>
+      
     </View>
   );
+
 }
+
+export default Matches;
