594ed98dd0ba3c4783401f6199e0f5105cd970a3
Nicolas <midia71@outlook.com>
2026-04-07 20:31:10 -0400

    Added modal when clicked on matched person.

--- DatingApp/app/(tabs)/matches.jsx
+++ DatingApp/app/(tabs)/matches.jsx
@@ -1,4 +1,11 @@
-import { Text, View, ScrollView, RefreshControl } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  RefreshControl,
+  Modal,
+  Image,
+} from "react-native";
 import { useState, useEffect, useCallback } from "react";
 import * as api from "../../util/api.js";
 import Button from "../../components/Button";
@@ -8,6 +15,22 @@ export default function MatchesScreen() {
   const [matches, setMatches] = useState([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [viewProfile, setViewProfile] = useState();
+
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birth.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  };
 
   const getMatchData = async () => {
     try {
@@ -67,18 +90,24 @@ export default function MatchesScreen() {
                 ? `${Math.min(100, Math.max(12, match.match_score))}%`
                 : "70%";
 
+              match.fillWidth = fillWidth;
               const fillPercent = parseFloat(fillWidth);
+              match.fillPercent = fillPercent;
               const r = Math.round(202 + (fillPercent / 100) * 53);
               const g = Math.round(202 - (fillPercent / 100) * 43);
               const b = Math.round(202 - (fillPercent / 100) * 1);
               const stripColor = `rgb(${r}, ${g}, ${b})`;
+              match.stripColor = stripColor;
 
               return (
                 <View key={index} style={style.matchCard}>
                   <Button
                     text=""
                     style={style.profileButton}
-                    onPress={() => {}}
+                    onPress={() => {
+                      setModalVisible(true);
+                      setViewProfile(match);
+                    }}
                   />
                   <View style={style.matchInfo}>
                     <Text style={style.matchName}>
@@ -107,6 +136,78 @@ export default function MatchesScreen() {
               );
             })}
       </ScrollView>
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <ScrollView>
+          <View style={style.modalContainer}>
+            <Button
+              icon={{ name: "close", size: 24 }}
+              style={style.closeButton}
+              onPress={() => setModalVisible(false)}
+            />
+            <Text style={style.modalTitle}>Viewing Profile</Text>
+            <View style={style.modalHeaderUnderline} />
+            <Image
+              source={{
+                uri: "https://reactnative.dev/img/tiny_logo.png",
+              }}
+              style={style.modalPicture}
+            />
+            <View style={style.modalContent}>
+              <Text style={style.matchScoreText}>
+                {viewProfile?.match_score}% Match Score
+              </Text>
+
+              <View style={style.matchScoreBar}>
+                <View
+                  style={[
+                    style.matchStrip,
+                    {
+                      width: viewProfile?.fillWidth,
+                      shadowColor: viewProfile?.stripColor,
+                      backgroundColor: viewProfile?.stripColor,
+                    },
+                  ]}
+                />
+              </View>
+              <View style={style.profileInfo}>
+                <Text style={style.profileName}>
+                  {viewProfile?.profile_data.FirstName +
+                    " " +
+                    viewProfile?.profile_data.LastName}
+                </Text>
+                <Text style={style.profileInfoText}>
+                  {viewProfile?.profile_data.Occupation}
+                </Text>
+                <Text style={style.profileMeta}>
+                  {viewProfile?.profile_data.Gender +
+                    " | " +
+                    calculateAge(viewProfile?.profile_data.DateOfBirth) +
+                    " | " +
+                    viewProfile?.profile_data.Sexuality}
+                </Text>
+              </View>
+              <View style={style.interests}>
+                <Text style={style.interestsHeader}>Interests</Text>
+                {viewProfile?.profile_data.user_interest.map(
+                  (interest, index) => {
+                    return (
+                      <View key={index} style={style.interestItem}>
+                        <Text style={style.interestText}>
+                          {interest.interests.name}
+                        </Text>
+                      </View>
+                    );
+                  },
+                )}
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </Modal>
     </View>
   );
 }
